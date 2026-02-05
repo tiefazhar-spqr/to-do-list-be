e44e1df45336436cfcfd9020c6c0ed88c131205f
tiefazhar-spqr <tiefazhar@gmail.com>
2026-02-05 10:26:04 +0700

    revisi 1

--- src/conn.js
+++ src/conn.js
@@ -1,4 +1,4 @@
-import { prismaClient } from "@prisma/client"
+import { prismaClient } from "../src/generated/prisma"
 
 const db = new prismaClient();
 export default db;
--- src/todos/todos.service.js
+++ src/todos/todos.service.js
@@ -3,7 +3,7 @@ import db from "../conn";
 
 export async function getTodo(req = request, res = response) {
     try {
-        const data = await db.Todo.findMany();
+        const data = await db.todo.findMany();
         return res.status(200).json({
             message: "success get data todos", data
         })
@@ -11,5 +11,26 @@ export async function getTodo(req = request, res = response) {
         return res.status(500).json({
             message: "invalid get data todos", error
         })
+    }
+}
+
+export const deletedToDo = async (req = request, res = response) => {
+    try {
+        const id = req.params.id
+        const intId = parseInt(id);
+
+        const res = await db.todo.delete({
+            where: {
+                // id : req.params.id 
+                id : intId
+            }
+        });
+
+        return res.status(200).json({
+            message: "deleted success", res
+        })
+
+        } catch (error) {
+        console.error(error.message);
     }
 }
